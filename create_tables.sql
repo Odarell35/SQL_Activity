9c86c2778a584d0e1e87bb54d965647b407cb0ff
Oradell35 <122868692+Odarell35@users.noreply.github.com>
2024-08-07 17:32:53 +0200

    Update create_tables.sql

--- create_tables.sql
+++ create_tables.sql
@@ -1,13 +1,17 @@
 -- A script that creates tables
 
 CREATE TABLE Books (
+    ID INT PRIMARY KEY AUTO_INCREMENT
     Title VARCHAR(255) NOT NULL,
     Author VARCHAR NOT NULL,
+    Author_ID INT,
     Description TEXT,
+    FOREIGN KEY (Author_ID) REFERENCES Authors(Author_ID)
 );
 
 -- Create table Authors
 CREATE TABLE Authors (
+    Author_ID INT PRIMARY KEY AUTO_INCREMENT,
     FirstName VARCHAR(255) NOT NULL,
     LastName VARCHAR(255) NOT NULL,
     Nationality ENUM('American', 'British', 'South_African', 'Other') NOT NULL
