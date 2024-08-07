60909e1f4f7845c8e445e1637e95320ddb0b8538
Oradell35 <122868692+Odarell35@users.noreply.github.com>
2024-08-07 17:47:56 +0200

    Update queries.sql

--- queries.sql
+++ queries.sql
@@ -1,3 +1,17 @@
 -- Queries
 
 SELECT COUNT(*) AS Bookstotal FROM Books;
+
+-- Find out which author has the most books
+SELECT Authors.FirstName, Authors.LastName, COUNT(Books.ID) AS BookCount
+FROM Authors
+JOIN Books ON Authors.Author_ID = Books.Author_ID
+GROUP BY Authors.Author_ID
+ORDER BY BookCount ASC
+LIMIT 1;
+
+-- Find out how many books there are according to nationality
+SELECT Authors.Nationality, COUNT(Books.ID) AS BookCount
+FROM Authors
+JOIN Books ON Authors.Author_ID = Books.Author_ID
+GROUP BY Authors.Nationality;
